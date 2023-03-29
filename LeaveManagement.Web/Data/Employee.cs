bf2966c96a79dd8f16236f8f3ce2254cc972ceef
gabriel.bauermann@gmail <gabriel.bauermann@gmail.com>
2023-03-29 14:27:36 -0300

    Add Leave tables and migrations

---
+++ LeaveManagement.Web/Data/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.Web.Data
+{
+    public class Employee: IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LasttName { get; set; }
+        public string TaxId { get; set; }
+        public int MyProperty { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
